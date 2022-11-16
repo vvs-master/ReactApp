8bfbbdf5d61b61f0a77e9d0e5a818e1309e2c771
vvs-master <79470174+vvs-master@users.noreply.github.com>
2022-11-16 17:58:51 +0300

    Update ExerciseBeginnerLevel.js

--- src/components/ExerciseBeginnerLevel.js
+++ src/components/ExerciseBeginnerLevel.js
@@ -1,5 +1,28 @@
-export default function ExerciseBeginnerLevel({data}){
+import Counter from "./Counter";
+export default function ExerciseBeginnerLevel({data, storageId}){
+    let compleatedStartCount = 0;
+    if(localStorage.getItem(storageId + '-beginner')){
+        compleatedStartCount = localStorage.getItem(storageId + '-beginner');
+    } else {
+        localStorage.setItem(storageId + '-beginner', 0)
+    }
+    
 return(
-    <div></div>
+    <div>
+        <h2>Exercise Beginner</h2>
+        <p>{data.beginnerOptionTitle}</p>
+        <p>{data.beginnerDescription}</p>
+        <ul>
+            {data.beginnerExercises.map((ex,i)=>(
+                <li key={i}>{ex.count} {ex.name}</li>
+            ))}
+        </ul>
+        <ul>
+            {data.beginnerGenders.map((ex,i)=>(
+                <li key={i}>{ex.gender} {ex.weight} {ex.unit}</li>
+            ))}
+        </ul>
+        <Counter compleatedStartCount={compleatedStartCount} storageId={storageId} level={'-beginner'}/>
+    </div>
 )
 }
